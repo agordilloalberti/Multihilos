9cd4dbdd7d9975d3fc4fecff048bdef1f0b91278
Alvarogogal <agorsua0610@g.educaand.es>
2024-12-10 21:36:43 +0100

    .

--- Multihilos/Program.cs
+++ Multihilos/Program.cs
@@ -1,3 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿public class main()
+{
+    public static void Main(string[] args)
+    {
+        Thread player1 = new Thread();
+        player1.Start();
+        Thread player2 = new Thread();
+        player2.Start();
+    }
 
-Console.WriteLine("Hello, World!");
+    private static String playRPS()
+    {
+        var random = new Random();
+        var r = random.Next(1,4);
+        return r switch
+        {
+            1 => "Piedra",
+            2 => "Papel",
+            _ => "Tijera"
+        };
+    }
+}
