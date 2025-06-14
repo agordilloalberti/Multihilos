44bb88e9ea87432c0b12b0e929ee59bc33e96985
Alvarogogal <agordillo@iesrafaelalberti.es>
2025-06-14 12:45:23 +0200

    Added explanation

--- FuerzaBruta/Program.cs
+++ FuerzaBruta/Program.cs
@@ -3,6 +3,7 @@ using CrackingPassword;
 
 /**
  * Explicacion sobre el funcionamiento
+ * Se ha probado0 con 4, 6, 8 10 hilos, el resultado mas veloz ha sido con 8 hilos.
  */
 
 const string path = @"..\..\..\2151220-passwords.txt";
@@ -11,7 +12,7 @@ var contraseñas = File.ReadAllLines(path).ToList();
 
 var random = new Random();
 var encripter = new Encripter();
-float Nhilos = 5;
+float Nhilos = 8;
 List<HiloBruteForce> hilos = new List<HiloBruteForce>();
 
 var password = contraseñas[random.Next(contraseñas.Count-1)];
