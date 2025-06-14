22cec99b2b500b34f5540efd23f0bd8c1b250a8b
Alvarogogal <agordillo@iesrafaelalberti.es>
2025-06-14 12:16:42 +0200

    Rebboted and restarted

---
+++ FuerzaBruta/HiloBruteForce.cs
@@ -0,0 +1,17 @@
+﻿namespace CrackingPassword;
+
+public class HiloBruteForce
+{
+    Thread Hilo;
+    public List<String> contraseñas = new List<string>();
+    
+    public void esperar()
+    {
+        Hilo.Join();
+    }
+
+    public String bruteForce(String password, Encripter encripter)
+    {
+        
+    }
+}
--- FuerzaBruta/Program.cs
+++ FuerzaBruta/Program.cs
@@ -1,24 +1,50 @@
 ﻿using System.Collections;
 using CrackingPassword;
 
+/**
+ * Explicacion sobre el funcionamiento
+ */
+
 const string path = @"..\..\..\2151220-passwords.txt";
 
 var contraseñas = File.ReadAllLines(path).ToList();
 
 var random = new Random();
 var encripter = new Encripter();
+float Nhilos = 4;
+List<HiloBruteForce> hilos = new List<HiloBruteForce>();
 
 var password = contraseñas[random.Next(contraseñas.Count-1)];
 
-var passworde = encripter.Encript(password);
+var passwordEncripted = encripter.Encript(password);
 
-Console.WriteLine(password+"  "+passworde);
+Console.WriteLine(password+"  "+passwordEncripted);
 
-foreach (var c in contraseñas)
+int cantidadContraseñas =int.Parse(Math.Floor(contraseñas.Count()/Nhilos).ToString());
+
+int inicio = 0;
+int final = cantidadContraseñas;
+
+for (int i = 0; i < Nhilos; i++)
 {
-    if (encripter.Encript(c)== passworde)
+    HiloBruteForce hilo = new HiloBruteForce();
+    for (int j = inicio; j < final; j++)
     {
-        Console.WriteLine("Lancontre\n"+c);
-        break;
+        hilo.contraseñas.Add(contraseñas[j]);
     }
+    hilos.Add(hilo);
+    inicio += cantidadContraseñas;
+    final += cantidadContraseñas;
+}
+
+//TODO: Make sure that, in case there is a rest, they are added to other threads
+if (contraseñas.Count()-cantidadContraseñas*Nhilos!=0)
+{
+    contraseñas.
+}
+
+
+foreach (var hilo in hilos)
+{
+    hilo.bruteForce(passwordEncripted, encripter);
 }
